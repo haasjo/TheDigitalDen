1bc25e8ad6daa87aa41bea7ef8d5e14149c4a0f7
sondresjolyst <sonyslyst@gmail.com>
2023-05-06 15:05:04 +0200

    move cart to right navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-function popUp() {
+function addToCart() {
     cart_number = document.getElementById("cart").innerHTML.toString()
     increase_cart_number = parseInt(cart_number) + 1
     document.getElementById("cart").innerHTML = increase_cart_number
